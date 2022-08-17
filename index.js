b62273c0d315276ad8373175a07aa0c8da1e8e61
mulukimwangeka <mwangekaruth@gmail.com>
2022-08-17 21:12:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+    function user(){
+        return currentUser;
+    }
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+    function messageforuser(){
+        return welcomeMessage;
+    }
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+    function excitedmessage(){
+        return excitedWelcomeMessage;
+    }
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+    function shortgreet(){
+        return shortGreeting;
+    }
+console.log(shortgreet());
+
